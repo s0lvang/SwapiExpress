60bfeed6edc791bbffc6418bb1f7d3b4907478f9
Paalar <paaledward@gmail.com>
2018-11-01 23:46:05 +0100

    Initates the config, seeders, models, migrations files

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
+// Set up the express app
 const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-mongoose.connect('mongodb://localhost/swapi')
-  .then(() => console.log('database connected'))
-  .catch(() => console.log('could not connect to database :('));
 
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+require('./server/routes')(app);
 
-const port = 8080;
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
-app.listen(port, () => console.log(`server running at ${port}`));
+module.exports = app;
