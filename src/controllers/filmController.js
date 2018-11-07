01c9b30512cd8842194d2bcaf94f61d8b65a17a5
Paalar <paaledward@gmail.com>
2018-11-07 13:48:45 +0100

    Partly creates controllers for all of backend

--- src/controllers/filmController.js
+++ src/controllers/filmController.js
@@ -1,1 +1,28 @@
-export default {};
+import db from '../models/index';
+
+const { Film } = db;
+const { Vehicle } = db;
+
+
+export default {
+  create(req, res) {
+    return Film
+      .create({
+        id: req.body.id,
+        title: req.body.title,
+        producer: req.body.producer,
+        episode_id: req.body.episode_id,
+        director: req.body.director,
+        release_date: req.body.release_date,
+        opening_crawl: req.body.opening_crawl,
+      })
+      .then(film => res.status(201).send(film))
+      .catch(error => res.status(400).send(error));
+  },
+  list(req, res) {
+    return Film
+      .all()
+      .then(film => res.status(200).send(film))
+      .catch(error => res.status(400).send(error));
+  },
+};
